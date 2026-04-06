f48f11c022279ccda5d35127c8fe7dfa7347ee70
lumm20 <moralf20@outlook.com>
2026-04-06 13:13:27 -0700

    feat: add update transaction action

--- src/app/(dashboard)/transactions/new/actions.ts
+++ src/app/(dashboard)/transactions/new/actions.ts
@@ -5,13 +5,17 @@ import { redirect } from "next/navigation"
 import { createClient } from "@/lib/supabase/server"
 import { transactionSchema } from "@/lib/validations/transaction"
 
-export async function createTransaction(formData: FormData) {
+async function getUser(){
   const supabase = await createClient()
 
   const { data: { user } } = await supabase.auth.getUser()
   if (!user) redirect("/login")
+  return {user, supabase}
+}
 
-  console.log("formdata:",Object.fromEntries(formData.entries()));
+export async function createTransaction(formData: FormData) {
+  const {user, supabase} = await getUser()
+
   // Parsear y validar con Zod
   const raw = {
     t_type: formData.get("t_type"),
@@ -41,11 +45,8 @@ export async function createTransaction(formData: FormData) {
   redirect("/transactions")
 }
 
-export async function eliminarMovimiento(id: string) {
-  const supabase = await createClient()
-
-  const { data: { user } } = await supabase.auth.getUser()
-  if (!user) redirect("/login")
+export async function deleteTransaction(id: string) {
+  const {user, supabase} = await getUser()
 
   const { error } = await supabase
     .from("transactions")
@@ -54,6 +55,46 @@ export async function eliminarMovimiento(id: string) {
     .eq("user_id", user.id)
 
   if (error) throw new Error("Error al eliminar")
+
+  revalidatePath("/transactions")
+  revalidatePath("/dashboard")
+  revalidatePath("/reports")
+}
+
+export async function updateTransaction(id: string, formData: FormData) {
+  const { user, supabase } = await getUser()
+
+  const { data: transaction } = await supabase
+    .from("transactions")
+    .select("*")
+    .eq("id", id)
+    .eq("user_id", user.id)
+    .single()
+
+  if (!transaction) throw new Error("Movimiento no encontrado")
+  
+  const raw = {
+    t_type: formData.get("t_type") ?? transaction.t_type,
+    category: formData.get("category") ?? transaction.category,
+    description: formData.get("description") ?? transaction.description,
+    amount: Number(formData.get("amount")) ?? transaction.amount,
+    t_date: formData.get("t_date") ?? transaction.t_date,
+    notes: formData.get("notes") ?? transaction.notes,
+  }
+
+  const parsed = transactionSchema.safeParse(raw)
+
+  if(!parsed.success) {
+    throw new Error(parsed.error.issues[0].message)
+  }
+
+  const { error } = await supabase
+    .from("transactions")
+    .update(parsed.data)
+    .eq("id", id)
+    .eq("user_id", user.id)
+
+  if (error) throw new Error("Error al actualizar el movimiento")
 
   revalidatePath("/transactions")
   revalidatePath("/dashboard")
